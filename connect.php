caf08e0cd856d7cac07bb1bb2d551eb500be5026
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-01-31 13:28:55 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,6 @@
 <?php
     // Create connection
-    $conn = pg_connect("host=ec2-52-208-145-55.eu-west-1.compute.amazonaws.com, 
-    user=jxjrhhnsbrdceq, 
-    password=26ef405f573a3c924900cf77a5fa8dc7d6a9e73499724dd79ab8bdd0bd475f93, 
-    dbname=d3jdf665veionf");
+    $conn = pg_connect("host=ec2-52-208-145-55.eu-west-1.compute.amazonaws.com user=jxjrhhnsbrdceq password=26ef405f573a3c924900cf77a5fa8dc7d6a9e73499724dd79ab8bdd0bd475f93 dbname=d3jdf665veionf");
 
     $templine = '';
     $lines = file("standardized_experience.sql");
