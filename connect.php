45119d92179a3f16e211054ab28b9c142b934d5c
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-01-31 13:43:53 -0500

    aaaaa

--- connect.php
+++ connect.php
@@ -2,17 +2,16 @@
     // Create connection
     $conn = pg_connect("host=ec2-52-208-145-55.eu-west-1.compute.amazonaws.com user=jxjrhhnsbrdceq password=26ef405f573a3c924900cf77a5fa8dc7d6a9e73499724dd79ab8bdd0bd475f93 dbname=d3jdf665veionf");
 
-    $templine = '';
-    $lines = file("standardized_experience.txt");
-
-    foreach ($lines as $line) {
-        if (substr($line, 0, 2) == '--' || $line == '')
-        continue;
-
-        $templine .= $line;
-        if (substr(trim($line), -1, 1) == ';') { 
-            pg_query($templine);
-            $templine = '';
+            pg_query("CREATE TABLE 'journeys' (
+                'subject' text DEFAULT 'Nenhuma',
+                'id' int(11) DEFAULT NULL,
+                'grade_1' int(11) DEFAULT NULL,
+                'grade_2' int(11) DEFAULT NULL,
+                'grade_reposition' int(11) DEFAULT NULL,
+                'grade_dissertation' int(11) DEFAULT NULL,
+                'exam' int(11) DEFAULT NULL,
+                'exam_reposition' int(11) DEFAULT NULL
+              )");
         };
     };
     // Check connection
