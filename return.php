1dbb17fe759711331ad58914aabb9f63c0a21edf
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-01-31 18:09:56 -0500

    Update return.php

--- return.php
+++ return.php
@@ -1,2 +1,2 @@
-<?php $_SESSION['subjecto'] = pg_fetch_assoc(pg_query($conn,'SELECT DISTINCT subject FROM quiz ORDER BY rand() LIMIT 1'));?>
+<?php $ara = pg_query($conn,'SELECT DISTINCT subject FROM quiz ORDER BY rand() LIMIT 1'); $_SESSION['subjecto'] = pg_fetch_assoc($ara);?>
 <div id="return"><a class="home" href="experience.php"><i class="fas fa-home"></i></a><a class="data" href="data.php"><i class="fas fa-file"></i></a><a class="reload" href="practice.php"><i class="fas fa-sync-alt"></i></a><a class="about" href="about.php"><i class="fas fa-question"></i></a></div>
