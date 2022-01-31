8e1e6ea1dcf4d1fc18340d99acaa11e2c4a68d67
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-01-31 13:13:36 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -11,7 +11,7 @@
 
         $templine .= $line;
         if (substr(trim($line), -1, 1) == ';') { 
-            pg_query($conn,$templine);
+            pg_query(pg_connect("username=host=ec2-52-208-145-55.eu-west-1.compute.amazonaws.com, user=jxjrhhnsbrdceq, password=26ef405f573a3c924900cf77a5fa8dc7d6a9e73499724dd79ab8bdd0bd475f93, database=d3jdf665veionf"),$templine);
             $templine = '';
         };
     };
