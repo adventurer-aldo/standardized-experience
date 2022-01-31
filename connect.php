d9b8b212a842d9bd63252401fb1a5b6a51188e6f
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-01-31 05:03:14 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,8 +3,5 @@
     $conn = pg_connect("username=host=ec2-52-208-145-55.eu-west-1.compute.amazonaws.com, user=jxjrhhnsbrdceq, password=26ef405f573a3c924900cf77a5fa8dc7d6a9e73499724dd79ab8bdd0bd475f93, database=d3jdf665veionf");
 
     // Check connection
-    if (!$conn) {
-        die("Connection failed: " . pg_connect_error());
-    }
     // <?php $aka = $conn->query("SELECT * FROM subject");$nume = pg_fetch_assoc($aka); echo "<li class='dashitem'>".implode("|",$nume)."</li>";
 ?>
