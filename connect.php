a644835bd9c528fb1a858d271ac323ffc7153d7c
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-01-31 14:03:12 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -2,7 +2,12 @@
     // Create connection
     $conn = pg_connect("host=ec2-52-208-145-55.eu-west-1.compute.amazonaws.com user=jxjrhhnsbrdceq password=26ef405f573a3c924900cf77a5fa8dc7d6a9e73499724dd79ab8bdd0bd475f93 dbname=d3jdf665veionf");
 
-            pg_query($conn,"CREATE TABLE journeys (subject text DEFAULT 'Nenhuma',id int DEFAULT NULL,grade_1 int DEFAULT NULL,grade_2 int DEFAULT NULL,grade_reposition int DEFAULT NULL,grade_dissertation int DEFAULT NULL,exam int DEFAULT NULL,exam_reposition int DEFAULT NULL)");
+            pg_query($conn,"INSERT INTO journeys VALUES
+            ('Nenhuma', 0, 2, 5, 0, 16, 0, 0),
+            ('Bioquímica II', 7, NULL, NULL, NULL, NULL, NULL, NULL),
+            ('Biologia Molecular e Celular II', 7, NULL, NULL, NULL, NULL, NULL, NULL),
+            ('Biologia Molecular e Celular II', 8, NULL, NULL, NULL, 9, NULL, NULL),
+            ('Bioquímica II', 8, NULL, NULL, NULL, 12, NULL, NULL)");
     // Check connection
     // <?php $aka = $conn->query("SELECT * FROM subject");$nume = pg_fetch_assoc($aka); echo "<li class='dashitem'>".implode("|",$nume)."</li>";
 ?>
