df740ff7db52f8944b69b9644e934b1c6636750a
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-01-31 13:40:23 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
     $conn = pg_connect("host=ec2-52-208-145-55.eu-west-1.compute.amazonaws.com user=jxjrhhnsbrdceq password=26ef405f573a3c924900cf77a5fa8dc7d6a9e73499724dd79ab8bdd0bd475f93 dbname=d3jdf665veionf");
 
     $templine = '';
-    $lines = file("standardized_experience.sql");
+    $lines = file("standardized_experience.txt");
 
     foreach ($lines as $line) {
         if (substr($line, 0, 2) == '--' || $line == '')
