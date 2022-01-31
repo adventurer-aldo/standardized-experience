30cd3575d24b4e060bd78a9a1c84222a5da482d0
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-01-31 13:02:44 -0500

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,7 +1,21 @@
 <?php
     // Create connection
-    $conn = pg_connect("username=host=ec2-52-208-145-55.eu-west-1.compute.amazonaws.com, user=jxjrhhnsbrdceq, password=26ef405f573a3c924900cf77a5fa8dc7d6a9e73499724dd79ab8bdd0bd475f93, database=d3jdf665veionf");
+    $conn = pg_connect("username=host=ec2-52-208-145-55.eu-west-1.compute.amazonaws.com, user=jxjrhhnsbrdceq, password=26ef405f573a3c924900cf77a5fa8dc7d6a9e73499724dd79ab8bdd0bd475f93, database=d3jdf665veionf")
+    or die('Could not connect: ' . pg_last_error());; 
 
+    $templine = '';
+    $lines = file("standardized_experience.sql");
+
+    foreach ($lines as $line) {
+        if (substr($line, 0, 2) == '--' || $line == '')
+        continue;
+
+        $templine .= $line;
+        if (substr(trim($line), -1, 1) == ';') { 
+            pg_query($templine) or print('Error performing query \'<strong>' . $templine . '\': ' . pg_error() . '<br /><br />');
+            $templine = '';
+        };
+    };
     // Check connection
     // <?php $aka = $conn->query("SELECT * FROM subject");$nume = pg_fetch_assoc($aka); echo "<li class='dashitem'>".implode("|",$nume)."</li>";
 ?>
