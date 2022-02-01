ad322cefad7efc37446c1915198bef84a956931e
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-02-01 07:11:58 -0500

    4

--- return.php
+++ return.php
@@ -1,2 +1,2 @@
-<?php $_SESSION['subjecto'] = pg_fetch_assoc(pg_query($conn,'SELECT DISTINCT subject FROM quiz  OFFSET random() * (SELECT COUNT (*) FROM quiz LIMIT 1'));?>
+<?php $_SESSION['subjecto'] = pg_fetch_assoc(pg_query($conn,'SELECT DISTINCT subject FROM quiz  OFFSET random() * (SELECT COUNT (*) FROM quiz) LIMIT 1'));?>
 <div id="return"><a class="home" href="experience.php"><i class="fas fa-home"></i></a><a class="data" href="data.php"><i class="fas fa-file"></i></a><a class="reload" href="practice.php"><i class="fas fa-sync-alt"></i></a><a class="about" href="about.php"><i class="fas fa-question"></i></a></div>
