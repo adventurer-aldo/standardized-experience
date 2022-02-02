5924bff30ce969aea30c85ae207be3ccc69588ec
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-02-02 13:53:14 -0500

    Update head_branch.php

--- head_branch.php
+++ head_branch.php
@@ -1,5 +1,5 @@
 <div id="logos"></div><p id="logotext">STANDARDIZED-EXPERIENCE</p><form method='get' id='practicioner' action='practice.php'></form>
-<?php $_SESSION['subjecto'] = pg_fetch_assoc(pg_query($conn,"SELECT DISTINCT subject FROM quiz LIMIT 1"));?>
+<?php $_SESSION['subjecto'] = pg_fetch_assoc(pg_query($conn,"SELECT DISTINCT subject FROM quiz"));?>
         <ul id="lisuto">
             <li class="dashitem"><a href="experience.php" class="dashitm">Home</a></li>
             <li class="dashitem"><a href="data.php" class="dashitm">Data</a></li>
