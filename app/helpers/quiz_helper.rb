f1bf27ee2d63b36251128fea53a3f4173a7bd190
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-03-04 19:14:13 -0500

    Update quiz_helper.rb

--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -1,3 +1,15 @@
 module QuizHelper
     
+    def makeQuiz(array)
+        @questionObjects = []
+        array.each do |answerID|
+            @answerObject = Answer.where(id: answerID)
+            @questionObjects << Question.where(id: @answerObject.questionid)
+        end
+        
+        @questionObjects.each do |quizQuestion|
+        end
+
+    end
+
 end
