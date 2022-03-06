a4391f964cb326ad44ef871062408a4bc45a0472
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-03-06 07:18:27 -0500

    Update quiz_helper.rb

--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -1,16 +1,3 @@
 module QuizHelper
-=begin
-
-            @questionObjects.each do |quizQuestion|
-                case eval("[#{quizQuestion.questiontype}]").sample
-                when :open
-                when :choice
-                when :multichoice
-                when :veracity
-                when :caption
-                when :table
-                when :template
-                end
-            end
-=end
+    
 end
