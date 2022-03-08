817dab984b8287dbbcd1689064ff6471d1c43868
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-03-08 00:30:57 -0500

    3.

--- app/helpers/quiz_helper.rb
+++ app/helpers/quiz_helper.rb
@@ -3,9 +3,10 @@ module QuizHelper
     def sort_choices
         @choices = []
         @qChoices = @question.choices.split('|')
-        rand(1..@qChoices.size).times { @choices << (@qChoices - @choices).sample}
+        rand(1..@qChoices.size).times { @choices << (@qChoices - @choices).sample }
         @choices << @question.answer
         @choices.shuffle!
+        return @choices
     end
 
 end
