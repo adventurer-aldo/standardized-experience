77620bdc271dc643ff2997c739f85abdefe0bf24
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-03-09 15:32:02 -0500

    sad

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     // Set the date we're counting down to
     var ehhh = new Date();
-    var countDownDate = new Date(ehhh.getTime() + 6*60000);
+    var countDownDate = new Date(ehhh.getTime() + quiz_time*60000);
 
 // Update the count down every 1 second
     var x = setInterval(function() {
@@ -13,18 +13,18 @@ $(document).ready(function(){
   var distance = countDownDate - now;
 
   // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  // var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-  document.getElementById("demo").innerHTML = minutes + "m " + seconds + "s ";
+  document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
 
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "Time's up!";
+    document.getElementById("timer").innerHTML = "Time's up!";
     document.getElementById("quiz").submit();
   } else if (minutes == 1 && seconds == 30) {
     var primeiro = document.getElementById('test1');
@@ -42,4 +42,8 @@ $(document).ready(function(){
     document.getElementById('theme').load();
   };
 }, 1000);
+});
+
+$('#quiz').onSubmit(function() {
+  document.getElementById("quiz").submit();
 });
