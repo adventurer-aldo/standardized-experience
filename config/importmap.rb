b95ca5a63a8bfb77bf14da45b1cee55d05d753f6
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-03-13 19:22:49 -0400

    9

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "application", preload: true
 
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascrip/components", under: "components"
