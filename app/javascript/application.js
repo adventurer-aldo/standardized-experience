77507eb1441beac7fef5ff3bed98ad41e24f0d6d
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-03-14 07:50:38 +0200

    R
    R

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,2 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "../../node_modules/@hotwired/turbo-rails*"
 import "./components/seconds*"
