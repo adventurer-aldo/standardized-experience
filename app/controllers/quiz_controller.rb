757c195945fcdfbfd11abe8d7fc1594a750f3a5f
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-03-20 09:10:57 -0400

    Update quiz_controller.rb

--- app/controllers/quiz_controller.rb
+++ app/controllers/quiz_controller.rb
@@ -84,8 +84,8 @@ class QuizController < ApplicationController
             allQuestions = baseQuery.limit(rand(5..10)) #)
         when 1
             allQuestions = baseQuery.where(%Q(level=1)).limit(rand(15..35))
-        when 2
-            allQuestions = baseQuery.where(%Q(level=1)).limit(rand(0..7)) + baseQuery.where(%Q(level=2)).limit(rand(10..28))
+        when 2 # baseQuery.where(%Q(level=1)).limit(rand(0..7)) + 
+            allQuestions = baseQuery.where(%Q(level=2)).limit(rand(10..28))
         when 3
             allQuestions = baseQuery.excluding(%Q(level=3)).limit(rand(10..40))
         when 4
