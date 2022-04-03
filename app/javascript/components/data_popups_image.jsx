d440bdd8fda3545e79357572228c206061c3727d
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-04-03 08:19:23 -0400

    css js

--- app/javascript/components/data_popups_image.jsx
+++ app/javascript/components/data_popups_image.jsx
@@ -2,3 +2,50 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
+
+ReactDOM.render(
+  <Popup
+    trigger={<button className="button"> Open Modal </button>}
+    modal
+    nested
+  >
+    {close => (
+      <div className="modal">
+        <button className="close" onClick={close}>
+          &times;
+        </button>
+        <div className="header"> Attach Image </div>
+        <div className="content">
+          {' '}
+          IMAGE FILE
+          <br />
+          Use previous image?<br />
+          Use image from set question:
+        </div>
+        <div className="actions">
+          <Popup
+            trigger={<button className="button"> Trigger </button>}
+            position="top center"
+            nested
+          >
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+              magni omnis delectus nemo, maxime molestiae dolorem numquam
+              mollitia, voluptate ea, accusamus excepturi deleniti ratione
+              sapiente! Laudantium, aperiam doloribus. Odit, aut.
+            </span>
+          </Popup>
+          <button
+            className="button"
+            onClick={() => {
+              console.log('modal closed ');
+              close();
+            }}
+          >
+            close modal
+          </button>
+        </div>
+      </div>
+    )}
+  </Popup>,
+document.getElementById("timer-example"))
