2cd7c6af4a1ba7c96b2bbbdf4587cb1d8c9ccff6
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-05-19 15:20:56 -0400

    s

--- app/javascript/controllers/paste_image.js
+++ app/javascript/controllers/paste_image.js
@@ -20,3 +20,26 @@ Object.defineProperty(String.prototype, 'capitalize', {
   },
   enumerable: false
 });
+
+;(function () {
+  var each = Array.prototype.forEach
+  var autoplayIds = []
+
+  document.addEventListener('turbo:before-cache', function () {
+    var autoplayElements = document.querySelectorAll('[autoplay]')
+    each.call(autoplayElements, function (element) {
+      if (!element.id) throw 'autoplay elements need an ID attribute'
+      autoplayIds.push(element.id)
+      element.removeAttribute('autoplay')
+    })
+  })
+
+  document.addEventListener('turbo:before-render', function (event) {
+    autoplayIds = autoplayIds.reduce(function (ids, id) {
+      var autoplay = event.data.newBody.querySelector('#' + id)
+      if (autoplay) autoplay.setAttribute('autoplay', true)
+      else ids.push(id)
+      return ids
+    }, [])
+  })
+})()
