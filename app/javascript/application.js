b8a377308f74048162600f9425ab80a2ab69eaae
adventurer-aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-06-02 22:30:53 -0400

    among us

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -42537,21 +42537,6 @@ For more info, visit https://reactjs.org/link/mock-scheduler`);
   });
 
   // app/javascript/controllers/paste_image.js
-  if (document.getElementById("question_image")) {
-    item = document.getElementById("question_image");
-    window.addEventListener("paste", (e) => {
-      item.files = e.clipboardData.files;
-      var img = new Image();
-      var bg = Array.from(e.clipboardData.items).find((x) => /^image\//.test(x.type));
-      if (bg) {
-        var blob = bg.getAsFile();
-        img.src = URL.createObjectURL(blob);
-      } else {
-        console.log("No image was pasted!");
-      }
-    });
-  }
-  var item;
   Object.defineProperty(String.prototype, "capitalize", {
     value: function() {
       return this.charAt(0).toUpperCase() + this.slice(1);
