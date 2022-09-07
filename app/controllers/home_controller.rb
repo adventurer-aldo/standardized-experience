ecd8bc30978f85347ec38e7f399ccfd8ddb835fd
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2022-09-07 19:03:25 +0200

    AJAX WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,37 +2,52 @@ class HomeController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @journey_check = Journey.where(id: Stat.last.journey_id).exists?
-    if @journey_check
-      @journey = Journey.last
-      sound = @journey.soundtrack
+    unless current_user.stat.journeys.empty?
+      @journey = current_user.stat.journeys.last
+      @progress = case @journey.level
+                  when 1
+                    'Primeiras avaliações'
+                  when 2
+                    'Segundas avaliações'
+                  when 3
+                    'Reposições'
+                  when 4
+                    'Realização de Trabalhos'
+                  when 5
+                    'Exames Normais'
+                  when 6
+                    'Exames de Recorrência'
+                  when 7
+                    'Resultados da Jornada'
+                  end
       @ost = case @journey.level
              when 0, 7
-               sound.home
+               @journey.soundtrack.home
              when 1
-               sound.preparations
+               @journey.soundtrack.preparations
              when 2, 3
-               sound.preparations_second
+               @journey.soundtrack.preparations_second
              when 4
-               sound.preparations_dissertation
+               @journey.soundtrack.preparations_dissertation
              when 5, 6
-               sound.preparations_exam
+               @journey.soundtrack.preparations_exam
              end
+
+      @start_time = @journey.start_time.to_time.to_f * 1000
+      @chairs = @journey.chairs.size
     end
 
     @ost = Soundtrack.first.home if @ost.nil?
-    @tip = if Subject.where(evaluable: 1).exists?
-             Subject.where(evaluable: 1).order(Arel.sql('RANDOM()')).limit(1).first.questions
-           else
-             Subject.all.order(Arel.sql('RANDOM()')).limit(1).first.questions
-           end.order(Arel.sql('RANDOM()')).limit(1)
+    @pop_quiz = (current_user.stat.evaluables.sample).questions
+                end.order(Arel.sql('RANDOM()')).limit(1)
 
-    quiz = Quiz.last
-
+    quiz = current_user.stat.quizzes.last
     @last_quiz = if quiz
-                   format('Seu último teste foi de %s, com nota %s. ', quiz.subject.title, helpers.grade(quiz).to_s.gsub('.',',')) + Misc::Text.evaluate(helpers.grade(quiz))
+                   format('Seu último teste foi de %s, com nota %s. ',
+                          quiz.subject.title,
+                          helpers.grade(quiz, true)) + Misc::Text.evaluate(helpers.grade(quiz))
                  else
-                   'Você ainda não fez nenhum teste. Comece um ou faça uma jornada!'
+                   'Você ainda não fez nenhum teste. Comece um!'
                  end
   end
 
@@ -45,141 +60,18 @@ class HomeController < ApplicationController
 
   def campaign; end
 
-  def question
-    if params[:operation] == 'delete'
-      Question.destroy_by(id: params[:id].to_i)
-      redirect_to question_path
-      return
-    end
-    @subjects = if Subject.where(evaluable: 1).exists?
-                  Subject.where(evaluable: 1)
-                else
-                  Subject.all
-                end.order(title: 'asc')
-    @tags = []
-    return unless Question.exists?
-
-    Question.all.map(&:tags).each do |tagging|
-      @tags += tagging
-    end
-    @tags.uniq!
-  end
-
   def subject
-    @subjects = Subject.all.order(title: :asc)
-  end
-
-  def submit_subject
-    case params[:operation]
-    when 'add'
-      Subject.create(title: params[:title], formula: params[:formula].to_i, difficulty: rand(1..5))
-    when 'evaluable'
-      Subject.find_by(id: params[:id].to_i).update(evaluable: params[:evaluability].to_i)
-    when 'edit'
-      Subject.find_by(id: params[:id].to_i).update(title: params[:title], formula: params[:formula].to_i)
-    when 'delete'
-      Subject.destroy_by(id: params[:id].to_i)
-    end
-    redirect_to("#{subject_path}#disciplinas")
-  end
-
-  def submit_question
-    types = %w[open choice multichoice veracity caption formula table match fill]
-    parameters = %w[strict order shambles]
-    last_question = Question.last
-    new_question = Question.create(question: params[:question],
-                                   question_types: eval(params[:types]).map { |i| types[i] },
-                                   answer: params[:answer],
-                                   subject_id: params[:subject],
-                                   level: params[:level].to_i,
-                                   tags: eval(params[:tags]),
-                                   parameters: eval(params[:parameters]).map { |i| parameters[i] }
-                                  )
-
-    if params[:reuse_image]
-      old_question = if params[:reuse_id] == '0' || Question.where(id: params[:reuse_id].to_i).exists? == false
-                       last_question
-                     else
-                       Question.find_by(id: params[:reuse_id].to_i)
-                     end
-      new_question.image.attach(old_question.image.blob) unless old_question.image.nil?
-      cookies[:reuse_image] = 'true'
-    else
-      new_question.image.attach(params[:image]) unless params[:image].nil?
-      cookies[:reuse_image] = 'false'
-    end
-
-    if params[:choices]
-      params[:choices].each do |_key, choice|
-        decoy = Choice.create(decoy: choice['text'], question_id: new_question.id)
-        next if choice['image'].nil? || choice['image'] == ''
-
-        decoy.image.attach(choice['image'])
-      end
-      cookies[:choices] = params[:choices].keys.size
-    else
-      cookies[:choices] = 0
-    end
-
-    cookies[:level] = params[:level]
-    cookies[:types] = params[:types]
-    cookies[:parameters] = params[:parameters]
-    cookies[:subject] = params[:subject]
-    cookies[:tags] = params[:tags]
-
-    redirect_to question_path
+    @auth_token = form_authenticity_token
+    @formulas = Array(0..4).map { |a| helpers.formula(a) }
   end
 
   def about; end
 
-  def cadeiras
-    File.open('cadeiras.json', 'w+') do |file|
-      str = '{'
-      Subject.all.each do |subject|
-        str << %(subjects[#{subject.id}] = "#{subject.title}";\n)
-      end
-      str << '}'
-      file.write(str)
-      send_file(file)
-    end
-  end
-
-  def questoes
-    File.open('questoes.json', 'w+') do |file|
-      str = '{"questions": ['
-      Question.all.order(id: :desc).limit(200).each do |question|
-        str << %({"id": #{question.id}, "subject": #{question.subject_id}, "level": #{question.level},
-        "types": #{question.question_types}, "question": "#{(question.question.length > 30) ? "#{question.question[0..30].gsub('"','\"')}..." : question.question}",
-        "answers": #{question.answer}, "tags": #{question.tags},
-        "image": "url('#{question.image.attached? ? question.image.url : 'https://i.ibb.co/dmtKHb0/black.png'}')",
-        "frequency": #{question.frequency}, "parameters": #{question.parameters}},)
-      end
-      str[-1] = ''
-      str << ']}'
-      file.write(str)
-      send_file(file)
-    end
-  end
-
-  def new_journey
-    journey = Journey.new(duration: 0, start_time: Time.now, soundtrack_id: Soundtrack.order(Arel.sql('RANDOM()')).limit(1).first.id)
-    journey.save
-    Subject.where(evaluable: 1).order(title: :asc).each do |subject|
-      chair = Chair.create(subject_id: subject.id, journey_id: journey.id, format: rand(0..1).round)
-      chair.update(dissertation: rand(0.0..20.0).round(2)) unless subject.questions.where(level: 3).exists?
-    end
-
-    Stat.last.update(journey_id: journey.id)
-    redirect_to root_path
-  end
-  
   def statistics
     @all = Question.all.size
     @stats = [0, 0, 0]
     Question.all.each do |question|
-      @stats[0] += question.frequency[0]
-      @stats[1] += question.frequency[1]
-      @stats[2] += question.frequency[2]
+      [0, 1, 2].each { |n| @stats[n] += question.frequency[n] }
     end
     @quizzes = Quiz.all.order(id: :desc).limit(10)
   end
@@ -194,12 +86,4 @@ class HomeController < ApplicationController
     @questions_pref = stat.questions_pref == 1 ? ' checked' : ''
     @theme = stat.theme_id
   end
-
-  def configure
-    Stat.last.update(
-      focus_level: params[:focus].to_i
-    )
-    redirect_to configurations_path
-  end
-
 end
