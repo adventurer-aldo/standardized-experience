0558bb3065c815e5f906c17a37e9ff141bf30534
Aldo <40561575+adventurer-aldo@users.noreply.github.com>
2023-05-25 17:09:43 +0300

    Delete test directory

--- test/mailers/se_mailer_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class SeMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/journey_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class JourneyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
